91d2b8853ec99cd746972fb5c5e84a45b0bf97bc
JhonaDev <jhona01230123@gmail.com>
2025-07-28 18:27:21 -0400

    implementacion de archivo csv y configurtacion de los input del usuario

---
+++ crud/test.py
@@ -0,0 +1,9 @@
+# prueba de slice con la palabra
+# pneumonoultramicroscopicsilicovolcanoconiosis
+
+word = "pneumonoultramicroscopicsilicovolcanoconiosis"
+print("la palabra con la que se realizara el ejercicio es: \n" + word + "\n ******")
+print(f"la primera palabra es \n {word[3:]} \n ******")
+print(f"la segunda palabra es \n {word[3:3]} \n ******")
+print(f"la tercera palabra es \n {word[:]} \n ******")
+print(f"la cuarta palabra es \n {word[1:-1:2]} \n *****")
